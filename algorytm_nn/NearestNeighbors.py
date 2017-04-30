80f05e0fd4c0d436b2d3a97081c8bdbeca5406dd
Anna Zawislak <annazaw15@gmail.com>
2017-04-30 15:33:25 +0200

    AZ: szkielet projketu

---
+++ algorytm_nn/NearestNeighbors.py
@@ -0,0 +1,27 @@
+import random
+import math
+import numpy
+
+
+class nearest_neighbourhood:
+
+    def odczytZPliku(sciezka):
+    pass
+
+    def numerowanieMiast(wspolrzedne_miast, N):
+    pass
+
+    def losowaniePunktuStartowego(N):
+    pass
+
+    def obliczanieOdleglosci(a, b, wspolrzedne_miast):
+    pass
+
+    def zapisDoPliku(wynik):
+    pass
+
+    def algorytmNN(sciezka):
+    pass
+
+
+nearest_neighbourhood.algorytmNN("TSP30.txt")
---
+++ algorytm_nn/test_nearest_neighbourhood.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_odczytZPliku(self):
+        self.fail()
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_numerowanieMiast(self):
+        self.fail()
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_losowaniePunktuStartowego(self):
+        self.fail()
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_obliczanieOdleglosci(self):
+        self.fail()
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_zapisDoPliku(self):
+        self.fail()
+
+
+class TestNearest_neighbourhood(TestCase):
+    def test_algorytmNN(self):
+        self.fail()
